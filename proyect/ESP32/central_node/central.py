143116fb8d4e09c812d3fa81f6ab2d385dbbbf76
Nicolas Cifuentes <nicolas.cifuentes01@correo.usa.edu.co>
2022-11-15 08:08:09 -0500

    Mac comparison fix

--- proyect/ESP32/central_node/central.py
+++ proyect/ESP32/central_node/central.py
@@ -1,6 +1,7 @@
 from time import sleep
 from umqtt.simple import MQTTClient
-import network, espnow
+import network
+import espnow
 
 sta = network.WLAN(network.STA_IF)
 sta.active(True)
@@ -8,7 +9,7 @@ sta.active(True)
 e = espnow.ESPNow()
 e.active(True)
 
-SERVER = 'xxx.xxx.xxx.xxx' # MQTT SERVER ADDRESS (raspberry)
+SERVER = 'xxx.xxx.xxx.xxx'  # MQTT SERVER ADDRESS (raspberry)
 CLIENT_ID = 'ESP32_DHT'
 TOPIC_TEMP = b'temperature'
 TOPIC_HYGRO = b'hygrometry'
@@ -17,19 +18,19 @@ TOPIC_LDR = b'luminosity'
 client = MQTTClient(CLIENT_ID, SERVER)
 client.connect()
 
-while True :
+while True:
     host, msg = e.recv()
     if msg:             # msg == None if timeout in recv()
         print(msg.decode())
         if msg == b'end':
             print('DONE')
             break
-        elif host.decode() == 'dht sender mac' :
+        elif host == b'dht sender mac':
             print('Temperature: {}'.format(msg.decode()))
             client.publish(TOPIC_TEMP, msg)
-        elif host.decode() == 'ldr sender mac' :
+        elif host == b'ldr sender mac':
             print('Luminosity: {}'.format(msg.decode()))
             client.publish(TOPIC_LDR, msg)
-        elif host.decode() == 'hygrometer sender mac' :
+        elif host == b'hygrometer sender mac':
             print('Hygrometry: {}'.format(msg.decode()))
             client.publish(TOPIC_HYGRO, msg)
